d11f9895a89ab4ab0b708d18d0f3b1c4244d21a9
Arnab Kumar Baishya <arnabbaishya2001@gmail.com>
2023-07-18 12:47:54 +0530

    css added and other changes done

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,12 +1,27 @@
-import {auth,provider} from "../firebase-config.js"
+import { auth, provider} from "../firebase-config.js"
 import { signInWithPopup } from "firebase/auth";
-export const Auth=()=>{
 
-    const signInWithgoogle=async()=>{
-        await signInWithPopup(auth,provider);
-    }
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
 
-    return (<div className="auth"><p>Sign in with Google</p>
-    <button onClick={signInWithgoogle}>Sign In With Google</button>
-    </div>);
+export const Auth = (props) => {
+    const {setIsAuth} = props;
+
+
+    const signInWithGoogle = async () => {
+        try {
+            const result = await signInWithPopup(auth, provider);
+            cookies.set("auth-token", result.user.refreshToken);
+            setIsAuth(true);
+        } catch(err) {
+            console.log(err);
+        }
+    };
+
+    return (
+        <div className="auth">
+            <p>Sign In With Google To Continue</p>
+            <button onClick={signInWithGoogle}> Sign In With Google</button>
+        </div>
+    );
 };
